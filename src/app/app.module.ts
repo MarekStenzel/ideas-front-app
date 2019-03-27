770b2860266d5393d4e4f4079b2d9919beda5790
MarekStenzel <stenzelmarek@gmail.com>
2019-03-27 15:34:16 +0100

    Added services

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { AuthService } from '@app/services/auth.service';
+import { ApiService } from '@app/services/api.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument()
+  ],
+  providers: [AuthService, ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
